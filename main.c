f03a761550afe308e121b791940364660cc7b426
TarannumPro <118145033+TarannumPro@users.noreply.github.com>
2023-02-23 04:39:25 +0000

    Initialised empty git

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "myhead.h"
+
+#define W 1
+#define A 2
+#define S 3
+#define D 4
+
+int main()
+{
+	printf("Initialising...\n");
+	return 0;
+}
